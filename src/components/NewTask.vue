34a511f503b7b8a75a11dab4d6d7dd4154c4674c
Andrea <andreagonzalo19@gmail.com>
2022-03-30 15:23:20 +0200

    edit

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -5,28 +5,44 @@
 
 <form class="mx-auto my-20 p-5 rounded-md bg-gray-100 shadow-lg w-3/4 justify-center flex flex-col items-center gap-y-5" action="" >
    <div class="flex flex-col sm:flex-row justify-center items-center my-5 gap-10 w-full"></div>
-   <label class="font-mono text-xl" for="name">New Todo</label>
+   <label class="font-mono text-xl" for="name">New Task</label>
+   
+   <!-- New task -->
+
    <input
       class="w-300px h-8 border border-gray-special/50 rounded p-2"
       v-model="newTodo"
-    />
-<button class="block w-full btn-template bg-blue-500 sm:inline sm:w-32 hover:bg-blue-600"
-        @click.prevent="addTodo">Add Task</button> 
+      placeholder="Put your fuck task"/>
+    <button class="block w-full btn-template bg-green-500 sm:inline sm:w-32 hover:bg-green-600" @click.prevent="addTodo">Add Task</button> 
   </form>
 
- <!-- <p v-if="newTodo.length < 4"  class="font-mono text-red-600 font-bold ml-10">
+ <!-- FALTA LLAMAR AL ERROR <p v-if="newTodo.length < 4"  class="font-mono text-red-600 font-bold ml-10">
       {{ newTodoErr }} </p> -->
 
   <div class="flex flex-col sm:flex-row gap-5 items-center justify-center my-10">
-    <section v-for="(todo, i) in datosTask" :key="'todo' + i">
-      <div>
-        <span>{{ todo.title }}</span>
-      </div>
-    </section>
+    
+    <div v-for="(todo, i) in datosTask" :key="'todo' + i">
+          <span :class="{completed: todo.is_complete}">{{ todo.title }}</span>
+          <div>
+
+            <!-- Edit button -->
+          <div>
+            <button @click="changeState">📝</button>
+            <input v-if="isEditing" v-model= "todo.title"/>
+            <button @click="saveEdit">Save</button>
+            </div>
+          
+          <button @click="removeTodo(todo)">🗑️</button>
+          
+           
+          <button ></button>
+          </div>
+    </div>
+
+    </div>
 
   </div>
 
-  </div>
 </template>
 
 <script setup>
@@ -34,14 +50,24 @@ import { useUserStore } from "../store/user";
 import { useTaskStore } from "../store/task";
 import { ref, computed } from 'vue';
 import { supabase } from "../supabase";
+import { useRoute } from "vue-router";
 
 const newTodo =ref("");
 const todos = ([]);
 const errorMsg = ref ("");
+let datosTask = ref([]);
+
 const taskStore = useTaskStore();
+const user = useUserStore();
+const router = useRoute();
+
+const isEditing = ref(false);
+const editingId = ref('');
+
+const isCompleted = ref(false)
 // const newTodoErr = ref("Please, write a word with more than three characters");
 
-let datosTask = ref([]);
+
 
 async function fetchAllTask(){
     const thisTask = await taskStore.fetchTasks();
@@ -50,12 +76,49 @@ async function fetchAllTask(){
 }
      fetchAllTask();
 
+// Add Task
 
 async function addTodo(){
     
     await taskStore.addTask(newTodo.value);
     await fetchAllTask();
+    console.log("hola");
     newTodo.value = "";
+}
+
+
+// async function editTodo(i) {
+//     isEditing.value = true;
+//     editingId.value = datosTask.value[i].id;
+//     newTodo.value = datosTask.value[i].title; 
+// }
+
+
+
+// Change State
+async function changeState() {
+  isEditing.value = true;
+}
+
+// Edit Task
+async function saveEdit(item){
+  await taskStore.editTask(item.title, item.id)
+  isEditing.value = false;
+  await fetchAllTask()
+}
+
+// Remove Task
+async function removeTodo(task) {
+  await taskStore.removeTodo(task.id);
+  await fetchAllTask()
+}
+
+//Complate Task
+async function completedTask(todo) {
+  const indexId = todo.id
+  todo.is_complete = !todo.is_complete
+  await taskStore.isComplete(indexId, todo.is_complete);
+  await fetchAllTask()
 }
 </script>
 
--- src/store/task.js
+++ src/store/task.js
@@ -7,8 +7,8 @@ export const useTaskStore = defineStore("tasks", {
     tasks: null,
   }),
 
-  // Take Tasks
   actions: {
+    // Take Tasks
     async fetchTasks() {
       const { data: tasks } = await supabase
         .from("tasks")
@@ -17,35 +17,55 @@ export const useTaskStore = defineStore("tasks", {
       return (this.tasks = tasks);
     },
 
-    // Insert Task
+    //Insert Task
 
     async addTask(title) {
-      // console.log(useUserStore().user.id);
-      const { data, error } = await supabase.from("tasks").insert([
+      const { error } = await supabase.from("tasks").insert([
         {
           user_id: useUserStore().user.id,
           title: title,
           is_complete: false,
         },
       ]);
+      if (error) throw error;
     },
 
     // Edit Task
 
+    async editTask(item, index) {
+      const { error } = await supabase
+        .from("tasks")
+        .update({ title: item })
+        .match({ id: index });
+    },
+
+    // Delete Task
+    async removeTodo(item) {
+      const { data, error } = await supabase
+        .from("tasks")
+        .delete()
+        .match({ id: item });
+      if (error) throw error;
+    },
+    // const { data, error } = await supabase
+    //         .from('tasks')
+    //         .update({ is_complete: bool })
+    //         .match({ id: id })
+    // },
+
     // async toggleDone(bool, id) {
-    //   const { data, error } = await supabase
-    //     .from("tasks")
-    //     .update({ is_complete: bool })
-    //     .match({ id: id });
+    //     const { data, error } = await supabase
+    //         .from('tasks')
+    //         .update({ is_complete: bool })
+    //         .match({ id: id })
     // },
 
     // async editTask(title, id) {
-    //   const { data, error } = await supabase
-    //     .from("tasks")
-    //     .update({ title: title })
-    //     .match({ id: id });
+    //     const { data, error } = await supabase
+    //         .from('tasks')
+    //         .update({ title: title })
+    //         .match({ id: id })
     // },
-
     // // Delete Task
 
     // async deleteTask(id) {
@@ -74,11 +94,10 @@ export const useTaskStore = defineStore("tasks", {
     // },
 
     // // Remove Task
-    // async removeAll() {
+    // async removeToodo() {
     //   const { data, error } = await supabase
     //     .from("tasks")
     //     .delete()
     //     .match({ user_id: useUserStore().user.id });
-    // },
   },
 });
